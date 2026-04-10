e2df1992a37a7430003870d12896c152127343c6
Jochen Heizmann <jochen@asylumsquare.com>
2026-04-10 20:53:31 +0200

    Update to Bullseye2D 2.0.0

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+import 'package:boing/boing_app.dart';
+import 'package:bullseye2d/bullseye2d.dart';
+
+void main() {
+  BoingApp(
+    config: AppConfig(
+      title: 'Boing',
+      width: 1280,
+      height: 720,
+    ),
+  );
+}
